26f3c2569001899c1bd10086de171a3af46db152
ledinhbaoute <20110613@student.hcmute.edu.vn>
2024-04-24 17:01:56 +0700

    prettier code,rename handle checkbox function, move logic handle checkbox to reducer, use createSelector instead, merge tasks and tmrtasks reducer

--- components/homepage/HomeScreen.js
+++ components/homepage/HomeScreen.js
@@ -2,128 +2,152 @@ import * as React from 'react';
 import { List, Checkbox } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useSelector, useDispatch } from 'react-redux';
+import { createSelector } from '@reduxjs/toolkit';
 import { View, Text, Button, TouchableOpacity, ScrollView } from 'react-native';
-import { fetchAllTasks, updateTasks } from '../../redux/TasksSlice';
-import { fetchAllTmrTasks, updateTmrTasks } from '../../redux/TomorrowTasksSlice';
-import { Bars3CenterLeftIcon, PlusCircleIcon } from "react-native-heroicons/solid";
+import {
+  fetchAllTasks,
+  updateTasks,
+  setTaskStatus,
+  fetchAllTmrTasks,
+  updateTmrTasks,
+  setTmrTaskStatus,
+} from '../../redux/TasksSlice';
+
+import {
+  Bars3CenterLeftIcon,
+  PlusCircleIcon,
+} from 'react-native-heroicons/solid';
 import { colors } from '../../theme/colors';
 import { formatTime } from '../../helper/TimeFormat';
 
 const HomeScreen = ({ navigation }) => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
-
-    const tasks = useSelector(state => state.tasks.listTasks);
-    const tmrTasks = useSelector(state => state.tmrTasks.listTasks);
-
-    const _handleCheckbox = (taskId) => {
-        // dispatch(markDone({ id: taskId }))
-        // console.log('tick')
-        const newListTasks = tasks.concat()
-        const selectedTask = tasks.find(task => task.id === taskId);
-        const index = newListTasks.findIndex(task => task.id === taskId)
-        newListTasks[index] = {
-            id: selectedTask.id, name: selectedTask.name,
-            time: selectedTask.time, isDone: !selectedTask.isDone
-        }
-        // console.log("update:", newListTasks)
-        dispatch(updateTasks(newListTasks))
+  const tasksSelector = createSelector(
+    state => state.tasks,
+    tasks => {
+      return tasks.listTasks;
     }
+  );
 
-    const _handleTmrCheckbox = (taskId) => {
-        const newListTasks = tmrTasks.concat()
-        const selectedTask = tmrTasks.find(task => task.id === taskId);
-        const index = newListTasks.findIndex(task => task.id === taskId)
-        newListTasks[index] = {
-            id: selectedTask.id, name: selectedTask.name,
-            time: selectedTask.time, isDone: !selectedTask.isDone
-        }
-        dispatch(updateTmrTasks(newListTasks))
+  const tasks = useSelector(tasksSelector);
+
+  const tmrTasksSelector = createSelector(
+    state => state.tasks,
+    tasks => {
+      return tasks.listTmrTasks;
     }
+  );
 
-    const _handleTask = (id) => {
-        navigation.navigate('EditTask', {
-            taskId: id
-        })
-    }
+  const tmrTasks = useSelector(tmrTasksSelector);
 
-    const _handleTmrTask = (id) => {
-        navigation.navigate('EditTmrTask', {
-            taskId: id
-        })
-    }
+  const markDoneTask = taskId => {
+    dispatch(setTaskStatus({ taskId, tasks }));
+  };
 
-    const _handlePressAddTask = () => {
-        navigation.navigate('AddTask');
-    }
+  const markDoneTmrTask = taskId => {
+    dispatch(setTmrTaskStatus({ taskId, tmrTasks }));
+  };
 
-    const _handlePressAddTmrTask = () => {
-        navigation.navigate('AddTmrTask');
-    }
+  const _handleTask = id => {
+    navigation.navigate('EditTask', {
+      taskId: id,
+    });
+  };
 
+  const _handleTmrTask = id => {
+    navigation.navigate('EditTmrTask', {
+      taskId: id,
+    });
+  };
 
+  const _handlePressAddTask = () => {
+    navigation.navigate('AddTask');
+  };
 
-    React.useEffect(() => {
-        dispatch(fetchAllTasks())
-        dispatch(fetchAllTmrTasks())
-    }, []
-    )
+  const _handlePressAddTmrTask = () => {
+    navigation.navigate('AddTmrTask');
+  };
 
+  React.useEffect(() => {
+    dispatch(fetchAllTasks());
+    dispatch(fetchAllTmrTasks());
+  }, []);
 
-    const renderTasks = tasks.map(task => (<List.Item key={task.id}
-        title={() => <Text className={task.isDone === true && 'line-through'}>{task.name}</Text>}
-        right={() => <Text>{formatTime(new Date(task.time))}</Text>}
-        left={() => <Checkbox status={task.isDone === true ? 'checked' : 'unchecked'}
-            color={colors.orange}
-            onPress={() => _handleCheckbox(task.id)} />}
-        onPress={() => _handleTask(task.id)} />)
-    )
+  const renderTasks = tasks.map(task => (
+    <List.Item
+      key={task.id}
+      title={() => (
+        <Text className={task.isDone === true && 'line-through'}>
+          {task.name}
+        </Text>
+      )}
+      right={() => <Text>{formatTime(new Date(task.time))}</Text>}
+      left={() => (
+        <Checkbox
+          status={task.isDone === true ? 'checked' : 'unchecked'}
+          color={colors.orange}
+          onPress={() => markDoneTask(task.id)}
+        />
+      )}
+      onPress={() => _handleTask(task.id)}
+    />
+  ));
 
-    const renderTmrTasks = tmrTasks.map(task => (<List.Item key={task.id}
-        title={() => <Text className={task.isDone === true && 'line-through'}>{task.name}</Text>}
-        right={() => <Text>{formatTime(new Date(task.time))}</Text>}
-        left={() => <Checkbox status={task.isDone === true ? 'checked' : 'unchecked'}
-            color={colors.orange}
-            onPress={() => _handleTmrCheckbox(task.id)} />}
-        onPress={() => _handleTmrTask(task.id)} />)
-    )
-    return (
-        <SafeAreaView>
-            <View className='container px-3'>
-                <View className="flex-row justify-between items-center">
-                    <Bars3CenterLeftIcon color={colors.black} size="30" />
-                </View>
-                <View className="flex-row justify-between items-center mt-4">
-                    <Text className="text-3xl font-bold">
-                        Today
-                    </Text>
-                    <TouchableOpacity>
-                        <PlusCircleIcon color={colors.orange} size="45" onPress={_handlePressAddTask}></PlusCircleIcon>
-                    </TouchableOpacity>
-                </View>
-                <ScrollView className="h-2/5">
-                    <List.Section>
-                        {renderTasks}
-                    </List.Section>
-                </ScrollView>
-                <View className="flex-row justify-between items-center mt-4">
-                    <Text className="text-3xl font-bold">
-                        Tomorrow
-                    </Text>
-                    <TouchableOpacity>
-                        <PlusCircleIcon color={colors.orange} size="45" onPress={_handlePressAddTmrTask}></PlusCircleIcon>
-                    </TouchableOpacity>
-                </View>
-                <ScrollView className="h-1/3">
-                    <List.Section>
-                        {renderTmrTasks}
-                    </List.Section>
-                </ScrollView>
-                
-            </View>
-
-        </SafeAreaView>
-    )
+  const renderTmrTasks = tmrTasks.map(task => (
+    <List.Item
+      key={task.id}
+      title={() => (
+        <Text className={task.isDone === true && 'line-through'}>
+          {task.name}
+        </Text>
+      )}
+      right={() => <Text>{formatTime(new Date(task.time))}</Text>}
+      left={() => (
+        <Checkbox
+          status={task.isDone === true ? 'checked' : 'unchecked'}
+          color={colors.orange}
+          onPress={() => markDoneTmrTask(task.id)}
+        />
+      )}
+      onPress={() => _handleTmrTask(task.id)}
+    />
+  ));
+  return (
+    <SafeAreaView>
+      <View className="container px-3">
+        <View className="flex-row justify-between items-center">
+          <Bars3CenterLeftIcon color={colors.black} size="30" />
+        </View>
+        <View className="flex-row justify-between items-center mt-4">
+          <Text className="text-3xl font-bold">Today</Text>
+          <TouchableOpacity>
+            <PlusCircleIcon
+              color={colors.orange}
+              size="45"
+              onPress={_handlePressAddTask}
+            ></PlusCircleIcon>
+          </TouchableOpacity>
+        </View>
+        <ScrollView className="h-2/5">
+          <List.Section>{renderTasks}</List.Section>
+        </ScrollView>
+        <View className="flex-row justify-between items-center mt-4">
+          <Text className="text-3xl font-bold">Tomorrow</Text>
+          <TouchableOpacity>
+            <PlusCircleIcon
+              color={colors.orange}
+              size="45"
+              onPress={_handlePressAddTmrTask}
+            ></PlusCircleIcon>
+          </TouchableOpacity>
+        </View>
+        <ScrollView className="h-1/3">
+          <List.Section>{renderTmrTasks}</List.Section>
+        </ScrollView>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default HomeScreen;
--- redux/TasksSlice.js
+++ redux/TasksSlice.js
@@ -1,68 +1,153 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { _storeTask, _retrieveTasks } from "../features/storage";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import {
+  _storeTask,
+  _retrieveTasks,
+  _storeTomorrowTask,
+  _retrieveTomorrowTasks,
+} from '../features/storage';
 
 export const fetchAllTasks = createAsyncThunk(
-    'tasks/fetchAllTasks',
-    async () => {
-        const response = await _retrieveTasks() || []
-        return response
-    },
-)
+  'tasks/fetchAllTasks',
+  async () => {
+    const response = (await _retrieveTasks()) || [];
+    return response;
+  }
+);
 
 export const updateTasks = createAsyncThunk(
-    'tasks/updateTasks',
-    async (tasks) => {
-        await _storeTask(tasks)
-        return tasks
-    },
-)
+  'tasks/updateTasks',
+  async tasks => {
+    await _storeTask(tasks);
+    return tasks;
+  }
+);
+
+export const fetchAllTmrTasks = createAsyncThunk(
+  'tasks/fetchAllTmrTasks',
+  async () => {
+    const response = (await _retrieveTomorrowTasks()) || [];
+    return response;
+  }
+);
+
+export const updateTmrTasks = createAsyncThunk(
+  'tasks/updateTmrTasks',
+  async tasks => {
+    await _storeTomorrowTask(tasks);
+    return tasks;
+  }
+);
+
+export const setTaskStatus = createAsyncThunk(
+  'task/setTaskStatus',
+  async (payload, thunkAPI) => {
+    const { taskId, tasks } = payload;
+    const newListTasks = tasks.concat();
+    const selectedTask = tasks.find(task => task.id === taskId);
+    const index = newListTasks.findIndex(task => task.id === taskId);
+    newListTasks[index] = {
+      id: selectedTask.id,
+      name: selectedTask.name,
+      time: selectedTask.time,
+      isDone: !selectedTask.isDone,
+    };
+    await _storeTask(newListTasks);
+    return newListTasks;
+  }
+);
+
+export const setTmrTaskStatus = createAsyncThunk(
+  'task/setTmrTaskStatus',
+  async (payload, thunkAPI) => {
+    const { taskId, tmrTasks } = payload;
+    const newListTasks = tmrTasks.concat();
+    const selectedTask = tmrTasks.find(task => task.id === taskId);
+    const index = newListTasks.findIndex(task => task.id === taskId);
+    newListTasks[index] = {
+      id: selectedTask.id,
+      name: selectedTask.name,
+      time: selectedTask.time,
+      isDone: !selectedTask.isDone,
+    };
+    await _storeTomorrowTask(newListTasks);
+    return newListTasks;
+  }
+);
 
 const initialState = {
-    listTasks: [],
-    isLoading: false,
-    isError: false,
-}
+  listTasks: [],
+  listTmrTasks: [],
+  isLoading: false,
+  isError: false,
+};
 
 const tasksSlice = createSlice({
-    name: "tasks",
-    initialState,
-    reducers: {
-        addTask(state, action) {
-            state.listTasks.push(action.payload)
-        },
-        markDone(state, action) {
-            const { id } = action.payload
-            const existingTask = state.listTasks.find(task => task.id === id)
-            console.log(existingTask)
-            if (existingTask) {
-                existingTask.isDone = true
-                console.log(existingTask)
-            }
-        }
+  name: 'tasks',
+  initialState,
+  reducers: {
+    addTask(state, action) {
+      state.listTasks.push(action.payload);
     },
-    extraReducers: (builder) => {
-        builder
-            .addCase(fetchAllTasks.fulfilled, (state, action) => {
-                state.listTasks = action.payload;
-                // console.log(action)
-            })
-            .addCase(fetchAllTasks.rejected, (state, action) => {
-                console.log('err', action)
-            })
-            .addCase(updateTasks.fulfilled, (state, action) => {
-                state.listTasks = action.payload;
-                console.log('add new task', action)
-            })
-            .addCase(updateTasks.rejected, (state, action) => {
-                console.log('err', action)
-            })
-    }
-})
+    markDone(state, action) {
+      const { id } = action.payload;
+      const existingTask = state.listTasks.find(task => task.id === id);
+      console.log(existingTask);
+      if (existingTask) {
+        existingTask.isDone = true;
+        console.log(existingTask);
+      }
+    },
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(fetchAllTasks.fulfilled, (state, action) => {
+        state.listTasks = action.payload;
+        // console.log(action)
+      })
+      .addCase(fetchAllTasks.rejected, (state, action) => {
+        console.log('err', action);
+      })
+      .addCase(updateTasks.fulfilled, (state, action) => {
+        state.listTasks = action.payload;
+        console.log('add new task', action);
+      })
+      .addCase(updateTasks.rejected, (state, action) => {
+        console.log('err', action);
+      })
+      .addCase(setTaskStatus.fulfilled, (state, action) => {
+        state.listTasks = action.payload;
+      })
+      .addCase(setTaskStatus.rejected, (state, action) => {
+        console.log('err', action);
+      })
+      .addCase(fetchAllTmrTasks.fulfilled, (state, action) => {
+        state.listTmrTasks = action.payload;
+        // console.log('action', action);
+      })
+      .addCase(fetchAllTmrTasks.rejected, (state, action) => {
+        console.log('err', action);
+      })
+      .addCase(updateTmrTasks.fulfilled, (state, action) => {
+        state.listTmrTasks = action.payload;
+        console.log('add new tomorrow task', action);
+      })
+      .addCase(updateTmrTasks.rejected, (state, action) => {
+        console.log('err', action);
+      })
+      .addCase(setTmrTaskStatus.fulfilled, (state, action) => {
+        state.listTmrTasks = action.payload;
+      })
+      .addCase(setTmrTaskStatus.rejected, (state, action) => {
+        console.log('err', action);
+      });
+  },
+});
 
-export const { addTask, markDone } = tasksSlice.actions
+export const { addTask, markDone } = tasksSlice.actions;
 
 export default tasksSlice.reducer;
 
-export const selectAllTasks = state => state.tasks.listTasks
+export const selectAllTasks = state => state.tasks.listTasks;
 
-export const selectTaskById = (state, taskId) => state.tasks.find(task => task.id === taskId)
+export const selectTaskById = (state, taskId) =>
+  state.tasks.find(task => task.id === taskId);
